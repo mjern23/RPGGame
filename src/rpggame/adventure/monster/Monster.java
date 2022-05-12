1c7f3e88d6ad7f8b4ece6f113540b5d0cf7669a7
alloalt55 <103965712+alloalt55@users.noreply.github.com>
2022-05-12 10:06:43 -0500

    I worked on monster

--- src/rpggame/adventure/monster/Monster.java
+++ src/rpggame/adventure/monster/Monster.java
@@ -1,14 +1,33 @@
 package rpggame.adventure.monster;
 
+import java.util.ArrayList;
+
 public class Monster {
 
-	String name; 
-	double health;
-	double damage;
+	private String name; 
+	private double health;
+	private double attack;
+	private double maxDamage, minDamage;
 	
-	public Monster(String name, double maxHealth, double minHealth, double DamageMult) {
+	public Monster(String name, double maxHealth, double minHealth, double maxDamage, double minDamage) {
 		this.name = name;
 		this.health = (Math.random() * maxHealth) + minHealth;
+		this.maxDamage = maxDamage;
+		this.minDamage = minDamage;
+//		Array[] monsterInfo = new ArrayList();
+//		monsterInfo.add(this.name);
+//		monsterInfo.add(this.health)
+//		monsterInfo.add(this.attack);
+	}
+	
+	public double getHealth() {
+		return this.health;
+	}
+	
+	public double attack() {
+		this.attack = (Math.random() * maxDamage) + minDamage;
+		
+		return attack;
 	}
 	
 }
