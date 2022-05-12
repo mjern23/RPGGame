ba5b5b19487641a4d6b2280ce29f5708cb817edd
EwMikey <33792731+EwMikey@users.noreply.github.com>
2022-05-11 23:40:08 -0500

    Added cooldown

--- src/rpggame/adventure/paths/BeginAdventure.java
+++ src/rpggame/adventure/paths/BeginAdventure.java
@@ -12,16 +12,19 @@ public class BeginAdventure {
 	public static Main m = new Main();
 
 	public static void introduceUser() {
-		System.out.printf("Professor Nick> Hello, %s. It's nice to meet you. My name is Professor Nick.%nProfessor Nick> I will appear here every"
-				+ " now and then to give you some tips along the way.%nProfessor Nick> Good luck on your journey, Adventurer.%n",
-				Adventurer.user().getName());
 		MethodHelp methodHelp = new MethodHelp();
+		System.out.printf("Professor Nick> Hello, %s. It's nice to meet you. My name is Professor Nick.",
+				Adventurer.user().getName()); 
+		methodHelp.cusWait(2);
+		System.out.printf("%nProfessor Nick> I will appear here every now and then to give you some tips along the way.");
+		methodHelp.cusWait(2);
+		System.out.printf("%nProfessor Nick> Good luck on your journey, Adventurer.%n");
 		methodHelp.cusWait(3);
 		pickStartPath();
 	}
 
 	public static void pickStartPath() {
-		System.out.printf("???> Hello, Adventurer. You have 3 choices here. Make sure to make your decision wisely.%n1) %n2) %n3)%n>> ");
+		System.out.printf("???> Hello, Adventurer. You have 3 choices here. Make your decision wisely.%n1) %n2) %n3)%n>> ");
 		MethodHelp methodHelp = new MethodHelp();
 		int choice = m.scan.nextInt();
 		methodHelp.cusWait(3);
--- src/rpggame/adventure/paths/paththree/PathThree.java
+++ src/rpggame/adventure/paths/paththree/PathThree.java
@@ -1,9 +1,12 @@
 package rpggame.adventure.paths.paththree;
 
+import rpggame.adventurer.Adventurer;
+
 public class PathThree {
 
 	public static void startPath() {
-		
+		System.out.println("You picked path 3!");
+		Adventurer.user().addPathChoice(3);
 	}
 	
 }
